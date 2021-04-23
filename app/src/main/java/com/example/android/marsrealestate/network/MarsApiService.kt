c4a19a29a84a830c6fbcd98de3608fea1e66ab98
leoapz <leoapaza2@gmail.com>
2021-04-22 23:04:12 -0300

    Connecting to the Internet

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,4 +17,31 @@
 
 package com.example.android.marsrealestate.network
 
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
 private const val BASE_URL = "https://mars.udacity.com/"
+
+private val retrofit = Retrofit.Builder()
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+/*
+* Create a MarsApiService interface, and define a getProperties() method to request the JSON response string.
+Annotate the method with @GET, specifying the endpoint for the JSON real estate response,
+* and create the Retrofit Call object that will start the HTTP request.
+* */
+interface MarsApiService {
+    @GET("realestate")
+    fun getProperties():
+            Call<String>
+}
+// Passing in the service API you just defined, create a public object called MarsApi
+// to expose the Retrofit service to the rest of the app:
+object MarsApi {
+    val retrofitService : MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}
